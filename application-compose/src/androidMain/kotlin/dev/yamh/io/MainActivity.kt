1b7d38ba49884300c428d54d6ffa8f70621a9017
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-07-24 16:31:46 +0300

    feat(YAMM-1): rename MainActivity.kt for improved project structure

--- application-compose/src/androidMain/kotlin/dev/yamh/io/MainActivity.kt
+++ application-compose/src/androidMain/kotlin/dev/yamh/io/MainActivity.kt
@@ -1,24 +1,13 @@
 package dev.yamh.io
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.home.matter.commissioning.CommissioningResult
 import com.google.home.HomeClient
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
-import org.koin.core.component.inject
 
 public class MainActivity : ComponentActivity() {
 
