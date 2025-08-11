7eec6c284c85f8ca8c96d22f0675265a6fae5c23
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-11 13:15:53 +0300

    feat(YAMM-1): add Kotlin serialization plugin and configure commonMain dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,26 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("dev.yamh.io.convention.application")
     id("dev.yamh.io.convention.di")
+    // TODO: fix
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
+kotlin {
+    sourceSets {
+        commonMain {
+            commonMain {
+                dependencies {
+                    implementation(libs.navigation.compose)
+
+                    implementation(projects.dataGhome)
+                    implementation(projects.presentationCoreNavigation)
+                    implementation(projects.presentationFeatureHomes)
+                    implementation(projects.presentationFeatureRooms)
+                }
+            }
+        }
+    }
+}
 
 dependencies {
 //    debugImplementation(compose.uiTooling)
