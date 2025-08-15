b059600d9ad7b7000f53e0200ab9c44c41d5d8ac
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-15 13:18:41 +0300

    feat(YAMM-1): add Id and Name value classes for core data representation

---
+++ data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/ghome/foobar/core/Id.kt
@@ -0,0 +1,8 @@
+package dev.yamh.io.data.ghome.ghome.foobar.core
+
+import kotlin.jvm.JvmInline
+
+@JvmInline
+public value class Id(
+    public val value: String
+)
---
+++ data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/ghome/foobar/core/Name.kt
@@ -0,0 +1,8 @@
+package dev.yamh.io.data.ghome.ghome.foobar.core
+
+import kotlin.jvm.JvmInline
+
+@JvmInline
+public value class Name(
+    public val value: String
+)
