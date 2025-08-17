ba1b6c6d412ba2df08c5f03b0bca571efa92612f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-17 13:21:52 +0300

    feat(YAMM-1): implement OnOffDeviceAttribute and DeviceModel classes for device control

--- data-ghome/build.gradle.kts
+++ data-ghome/build.gradle.kts
@@ -1,12 +1,18 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     id("dev.yamh.io.convention.library")
     id("dev.yamh.io.convention.di")
 }
 
+val secretsProperties = Properties().apply {
+    load(File(project.rootDir, "configure/secrets.properties").inputStream())
+}
+
+val projectNumber = secretsProperties["PROJECT_NUMBER"] as String
 
 dependencies {
     // Home API SDK dependency:
@@ -14,3 +20,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-home-types:17.0.0")
 }
 
+
+android {
+    defaultConfig {
+        buildConfigField("String", "GOOGLE_CLOUD_PROJECT_ID", projectNumber)
+    }
+}
