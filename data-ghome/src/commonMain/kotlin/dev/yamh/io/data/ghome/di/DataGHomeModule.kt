c80dd574e1c05fb382468a66cc1ebd23b7cf8506
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-08-20 17:24:36 +0300

    feat: add MainViewController for Compose UI integration

--- data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/di/DataGHomeModule.kt
+++ data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/di/DataGHomeModule.kt
@@ -2,4 +2,4 @@ package dev.yamh.io.data.ghome.di
 
 import org.koin.core.module.Module
 
-public expect val gHomeModule: Module
+public expect val dataGHomeModule: Module
--- data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/ghome/foobar/core/Name.kt
+++
@@ -1,8 +0,0 @@
-package dev.yamh.io.data.ghome.ghome.foobar.core
-
-import kotlin.jvm.JvmInline
-
-@JvmInline
-public value class Name(
-    public val value: String
-)
