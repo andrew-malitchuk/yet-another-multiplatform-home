b1e4e5f55abc6ab48f50c407f7ef6f8a6b94edde
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-10-29 19:19:07 +0200

    feat: add support for WindowCovering device type in HomeClientEntity; enhance device attribute handling

--- data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/source/datasource/room/RoomModel.kt
+++ data-ghome/src/commonMain/kotlin/dev/yamh/io/data/ghome/source/datasource/room/RoomModel.kt
@@ -1,14 +1,15 @@
 package dev.yamh.io.data.ghome.source.datasource.room
 
+import dev.yamh.common.core.core.Color
 import dev.yamh.common.core.core.Id
 import dev.yamh.common.core.core.Name
 import dev.yamh.io.data.ghome.source.datasource.device.DeviceModel
 
-public expect class RoomModel(id: Id, name: Name, homeId: Id) {
-
+public expect class RoomModel(id: Id, name: Name, homeId: Id, color: Color) {
     public val id: Id
     public val name: Name
     public val homeId: Id
+    public val color: Color
 
     public suspend fun getDevices(): List<DeviceModel>
     public suspend fun getDevice(id: Id): DeviceModel?
