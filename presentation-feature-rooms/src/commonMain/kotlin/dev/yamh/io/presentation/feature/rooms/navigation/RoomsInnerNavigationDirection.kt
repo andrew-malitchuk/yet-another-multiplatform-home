a1f151cedd59a3765ee88864a1dc8eea738157bb
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-11-29 19:42:49 +0200

    feat: add compact, expanded, and vertical fold views for settings feature

--- presentation-feature-rooms/src/commonMain/kotlin/dev/yamh/io/presentation/feature/rooms/navigation/RoomsInnerNavigationDirection.kt
+++
@@ -1,9 +0,0 @@
-package dev.yamh.io.presentation.feature.rooms.navigation
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-public sealed class RoomsInnerNavigationDirection {
-    @Serializable
-    public data object Rooms : RoomsInnerNavigationDirection()
-}
