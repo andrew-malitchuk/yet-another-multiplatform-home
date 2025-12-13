e2e4f0d5e9c9ecd4afb979b17e88de943f599e7b
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-12-13 19:53:38 +0200

    feat: add Color extension function and glitch effect modifier

--- presentation-core-ui/src/commonMain/kotlin/dev/yamh/io/presentation/core/ui/core/window/WindowSizeHelper.kt
+++ presentation-core-ui/src/commonMain/kotlin/dev/yamh/io/presentation/core/ui/core/window/WindowSizeHelper.kt
@@ -4,15 +4,27 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import dev.yamh.io.presentation.core.platform.source.device.DeviceEnvironment
+import dev.yamh.io.presentation.core.platform.source.device.DeviceEnvironment.getOrientation
+import dev.yamh.io.presentation.core.platform.source.device.DeviceEnvironment.getPosture
+import dev.yamh.io.presentation.core.platform.source.device.DeviceOrientation
+import dev.yamh.io.presentation.core.platform.source.device.DevicePosture
+import kotlin.getValue
 
 
 public class WindowSizeHelper {
+
+
     public companion object {
+
 
         @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
         @Composable
         public fun getWindowWidthSizeClass(): WindowSize {
             val windowSizeClass = calculateWindowSizeClass()
+
+            println("size | ${windowSizeClass.widthSizeClass} | ${windowSizeClass.heightSizeClass}")
+
             return when (windowSizeClass.widthSizeClass) {
                 WindowWidthSizeClass.Compact -> WindowSize.COMPACT
                 WindowWidthSizeClass.Medium -> WindowSize.MEDIUM
@@ -21,18 +33,24 @@ public class WindowSizeHelper {
             }
         }
 
+
         @Composable
         public fun DetectSize(
-            compact: @Composable () -> Unit,
-            medium: @Composable () -> Unit,
-            expanded: @Composable () -> Unit
+            compact: @Composable (DeviceOrientation, DevicePosture) -> Unit,
+            medium: @Composable (DeviceOrientation, DevicePosture) -> Unit,
+            expanded: @Composable (DeviceOrientation, DevicePosture) -> Unit
         ) {
-            when (getWindowWidthSizeClass()) {
-                WindowSize.COMPACT -> compact()
-                WindowSize.MEDIUM -> medium()
-                WindowSize.EXPANDED -> expanded()
+            val windowSize = getWindowWidthSizeClass()
+            val deviceOrientation = getOrientation()
+            val posture = getPosture()
+
+            println("device: ${windowSize} | ${deviceOrientation} | ${posture}")
+
+            when (windowSize) {
+                WindowSize.COMPACT -> compact(deviceOrientation, posture)
+                WindowSize.MEDIUM -> medium(deviceOrientation, posture)
+                WindowSize.EXPANDED -> expanded(deviceOrientation, posture)
             }
         }
     }
 }
-
