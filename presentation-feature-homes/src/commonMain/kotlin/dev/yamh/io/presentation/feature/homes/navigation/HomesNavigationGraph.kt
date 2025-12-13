4912907c54ba4ae4125824e8bdbc724b96cfada1
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-12-13 19:53:14 +0200

    feat: add Koin module for HomesViewModel dependency injection

--- presentation-feature-homes/src/commonMain/kotlin/dev/yamh/io/presentation/feature/homes/navigation/HomesNavigationGraph.kt
+++ presentation-feature-homes/src/commonMain/kotlin/dev/yamh/io/presentation/feature/homes/navigation/HomesNavigationGraph.kt
@@ -4,11 +4,11 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import dev.yamh.io.presentation.core.navigation.GlobalNavigation
+import dev.yamh.io.presentation.core.navigation.source.navigation.GlobalNavigation
 import dev.yamh.io.presentation.feature.homes.homes.HomesScreen
 
 context(NavHostController)
-public fun NavGraphBuilder.homesNavigationGraph() {
+public fun NavGraphBuilder.homesNavigationGraph():Unit = with(this@NavHostController) {
     navigation<GlobalNavigation.Homes>(
         startDestination = HomesInnerNavigationDirection.Homes,
     ) {
