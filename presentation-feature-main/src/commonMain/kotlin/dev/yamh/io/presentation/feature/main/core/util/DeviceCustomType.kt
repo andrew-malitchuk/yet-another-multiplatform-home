4c442e658bae4a773ee94cf7f35955456673e6a3
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-12-20 19:52:11 +0200

    feat: add DeviceCustomType utility and update build.gradle.kts dependencies

---
+++ presentation-feature-main/src/commonMain/kotlin/dev/yamh/io/presentation/feature/main/core/util/DeviceCustomType.kt
@@ -0,0 +1,10 @@
+package dev.yamh.io.presentation.feature.main.core.util
+
+import dev.yamh.domain.core.source.model.device.type.DeviceCustomType
+import dev.yamh.io.presentation.core.ui.source.kit.molecule.bar.DeviceType
+
+public fun DeviceCustomType.toDeviceType(): DeviceType? {
+    return DeviceType.entries.firstOrNull {
+        it.typeName == this.typeName
+    }
+}
