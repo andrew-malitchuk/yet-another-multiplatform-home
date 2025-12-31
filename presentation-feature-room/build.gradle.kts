3daffaf4b1d74ec6995ff3854922e513f3021e60
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2025-12-31 19:50:22 +0200

    feat: add DeviceCustomType conversion and update build.gradle.kts dependencies

---
+++ presentation-feature-room/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
+
+plugins {
+    id("dev.yamh.io.convention.feature")
+    id("dev.yamh.io.convention.di")
+        alias(libs.plugins.kotlin.serialization)
+}
+
+
+kotlin {
+    sourceSets{
+        commonMain.dependencies {
+            implementation(libs.navigation.compose)
+            implementation(libs.kotlinx.serialization.core) // Use the latest version
+            implementation(libs.kotlinx.serialization.json) // Use the latest version
+            implementation(projects.commonCore)
+            implementation(projects.domainCore)
+            implementation(projects.domainUsecase)
+            implementation(projects.presentationCoreStyling)
+            implementation(projects.presentationCoreUi)
+            implementation(projects.presentationCoreLocalisation)
+            implementation(projects.presentationCorePlatform)
+            implementation(projects.presentationCoreNavigation)
+
+            implementation(libs.connectivity.core)
+            implementation(libs.connectivity.device)
+
+            // For compose support
+            implementation(libs.connectivity.compose.device)
+        }
+    }
+}
+
